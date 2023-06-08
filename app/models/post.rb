70036dad36306e50f37a3937ef3f65f15dc3e0f2
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-06-08 12:20:37 +0200

    Modify the counter methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,10 @@ class Post < ApplicationRecord
   has_many :likes
 
   def update_user_post_counter
-    author.increment!(:post_counter)
+    post_count = author.posts.count
+    author.update_column(:post_counter, post_count.zero? ? 0 : post_count)
   end
-  
+
   def five_most_recent_comments
     comments.order(created_at: :desc).limit(5)
   end
