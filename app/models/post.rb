d957595f64d7fe182eb6e1fa2db637c152bbb95b
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-06-08 16:40:40 +0200

    Add validations to modals

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,19 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
+  def five_most_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
   def update_user_post_counter
     post_count = author.posts.count
     author.update_column(:post_counter, post_count.zero? ? 0 : post_count)
   end
 
-  def five_most_recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
 end
