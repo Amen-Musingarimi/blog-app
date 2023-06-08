729fc207faa221a6e37bc9efdecc92f5962b450a
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-06-09 00:21:05 +0200

    Add rspec tests

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe Comment, type: :model do
+  user = User.new(name: 'Something',
+                  photo: 'http://localhost:3000/something.jpg',
+                  bio: 'Something test',
+                  post_counter: 0)
+  post = Post.new(
+    title: 'Something',
+    text: 'Something test',
+    author: user,
+    comments_counter: 0,
+    likes_counter: 0
+  )
+
+  subject(:comment) do
+    Comment.new(text: 'TEst comments',
+                author: user,
+                post: post)
+  end
+  it 'is valid with valid attributes' do
+    expect(comment).to be_valid
+  end
+  it 'it is  valid without a comment' do
+    comment.text = nil
+    expect(comment).to be_valid
+  end
+
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  it "is not valid without a title" do
+    post = Post.new
+    expect(post).to_not be_valid
+  end
+end
