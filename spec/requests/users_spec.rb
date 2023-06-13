b1ea56a0678bfd04659abfa9c90aed8c14049cce
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-06-13 16:18:10 +0200

    Add controllers specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'User GET/ index' do
+    let!(:user) do
+      User.create(
+        name: 'Anything',
+        photo: 'http://localhost:3000/anything.jpg',
+        bio: 'Anything test',
+        post_counter: 0
+      )
+    end
+
+    it 'returns user HTTP success' do
+      get '/users/'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders user template' do
+      get '/users/'
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "User GET /show'" do
+    let!(:user) do
+      User.create(
+        name: 'John Doe',
+        photo: 'http://localhost:3000/anything.jpg',
+        bio: 'John Doe test',
+        post_counter: 0
+      )
+    end
+
+    it 'returns success for detail user' do
+      get "/users/#{user.id}"
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders user detail template' do
+      get "/users/#{user.id}"
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      get user_path(user)
+      expect(response.body).to include('User Details')
+    end
   end
 end
