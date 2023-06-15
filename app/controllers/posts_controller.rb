598ef7394dc7563163e194b7534e7ba97c42477e
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-06-15 21:42:50 +0200

    Add forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,29 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.includes(:comments)
-
-    respond_to do |format|
-      format.html
-      format.xml { render xml: @posts }
-      format.json { render json: @posts }
-    end
+    @posts = @user.posts
   end
 
   def show
-    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
-    respond_to do |format|
-      format.html
-      format.xml { render xml: @post.comments }
-      format.json { render json: @post.comments }
+  end
+
+  def new
+    @post = @current_user.posts.new
+  end
+
+  def create
+    @post = @current_user.posts.new(post_params)
+    if @post.save
+      redirect_to user_posts_path(@current_user)
+    else
+      render :new
     end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
