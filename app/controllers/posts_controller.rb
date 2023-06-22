4c26c90cd5e7bcdade553f87080b257dc80b3eb6
Amen-Musingarimi <takudzwamusinga@gmail.com>
2023-06-22 19:03:10 +0200

    Add cancan authorisation

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,26 +1,33 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
 
   def new
-    @current_user = current_user
-    @post = @current_user.posts.new
+    @post = current_user.posts.new
   end
 
   def create
-    @current_user = current_user
-    @post = @current_user.posts.new(post_params)
+    @post = current_user.posts.new(post_params)
     if @post.save
-      redirect_to user_posts_path(@current_user)
+      redirect_to user_posts_path(current_user)
     else
       render :new
     end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    @post.destroy
+    redirect_to user_posts_path(@user)
   end
 
   private
