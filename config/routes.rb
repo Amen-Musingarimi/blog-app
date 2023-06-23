d258b9c4e0ec60c2c0e4ab48249d9dcc4e9a74a1
Amen Musingarimi <92303637+Amen-Musingarimi@users.noreply.github.com>
2023-06-23 16:08:04 +0200

    Modify routes file

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'comments/index'
-      get 'comments/create'
+      post 'comments/create'
       get 'posts/index'
       resources :users, only: [] do
         resources :posts, only: [:index] do
